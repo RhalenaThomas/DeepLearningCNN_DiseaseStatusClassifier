f34a30552bfb2bb077b89e55f5216f3361eeeab7
Sneha Shinde <71280646+xosneha@users.noreply.github.com>
2021-03-19 18:53:48 -0400

    Added csv output to batch tests

--- batch_tester.py
+++ batch_tester.py
@@ -1,4 +1,5 @@
 from test_model import test
+import pandas as pd 
 
 def main():
     # list of models to run
@@ -7,7 +8,9 @@ def main():
     test_set_li = ["batch_1_3a", "batch_1_3b", "batch_1_4", "batch_2_3a", "batch_2_3b", "batch_3_a", "batch_3_b", "batch_3_a_b", "batch_3a_4", "batch_3b_4", "batch_4_plate_1", "batchfive", "XCL_NPC_batch_1", "XCL_NPC_batch_2", "batch1and2"]
 
     file = "results.txt" # text file to append results to
-
+    csv_path = "" # csv file to append accuracies to 
+    
+    result_li=[]
     for model in model_li:
         for test_set in test_set_li:
             acc, report = test(test_set, model)
@@ -15,8 +18,15 @@ def main():
             f.write("the accuracy for model " + model + " and test set " + test_set + " is " + str(acc) + "\n")
             f.write(report + "\n")
             f.write("..............................................................................\n")
-
+            result_li.append([model, test_set, str(acc)])
             f.close()
+
+    convert_dict_to_csv(result_li, csv_path)
+
+def convert_dict_to_csv(output, csv_path):
+    df = pd.DataFrame.from_dict(output)
+    df.index = df.index + 1
+    result = df.to_csv(path_or_buf=csv_path, header=['Model', 'Test set', 'Accuracy'],  index=False)
 
 if __name__ == '__main__':
    main()
