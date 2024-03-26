959ff8aa8f73bfda6af8bd725e0a45c1230c9f56
RhalenaThomas <rhalena.t@gmail.com>
2024-03-26 13:48:56 -0400

    setting up workbook to test image cleaning code

---
+++ NotesOnFileOrganization.sh
@@ -0,0 +1,13 @@
+# number of files in a folder
+
+find /data/Durcanlab/CNN_proj/preprocessed_CX5_A/unhealthy -maxdepth 1 -type f | wc -l
+
+find /data/Durcanlab/CNN_proj/cropGridnew -maxdepth 1 -type f | wc -l
+
+# takes a long time if there are a lot of files 
+
+copy some processed images locally
+
+
+
+
